561b313446d752bbe048b5c697ae1727c892ff49
Yuvraj Singh Rajput <75319329+yuvrajsr2@users.noreply.github.com>
2025-10-23 18:53:24 -0400

    Removed upload route and combined with analyze route and also built the file processing for pdf

--- backend/src/routes/analyze.route.js
+++ backend/src/routes/analyze.route.js
@@ -1,16 +1,20 @@
 import express from 'express';
+import multer from 'multer';
 import {upload} from '../utils/uploadHandle.js';
+import pdfParse from 'pdf-parse';
+import axios from 'axios';
+
 
 
 const router = express.Router();
 
 
 // upload uysing multer
-router.post('/upload', upload, (req, res) =>{
-    console.log(req.files);
-    res.json({message:"Files uploaded ", files:req.files})
-})
-
+// router.post('/upload', upload, (req, res) =>{
+//     console.log(req.files);
+//     res.json({message:"Files uploaded ", files:req.files})
+// });
+// im gonna combine upload and analyze in one route only 
 
 
 // analyze endpoint:todo using hugging face api to analyze the resume and job description
@@ -18,8 +22,48 @@ router.post('/upload', upload, (req, res) =>{
 // process them accordingly and use the hugging face api to analyze them. and then return 
 // the analysis result as json response. the response should be a percetange match 
 //and the the key skills matched and not matched and give recommendation to improve the resume.
-router.post('/analyze', (req, res) =>{
-    res.send("Analyze endpoint");
+router.post('/analyze', upload, async (req, res) =>{
+
+
+    if (!req.files || !req.files["resume"] || !req.files["jobDescription"]){
+        return res.status(400).json({error:"Invalid plz enter both files gang"})
+    }
+
+
+    const resume = req.files['resume'][0];
+    const jobDes = req.files['jobDescription'][0];
+
+
+    const extractData = async (data)=>{
+        if (data.mimetype === "application/pdf"){
+            const pdf = await pdfParse(data.buffer);
+            return pdf.text;
+        }
+        else{
+            return data.buffer.toString("utf-8");
+        }
+    }
+
+
+    const resumeData = await extractData(resume);
+    const jobData = await extractData(jobDes);
+
+
+
+    //todo: get hugging face api, then send a axios req to hugging face with the prompt
+    
+
+
+    // return formated res
+
+   
+
+
+
+    res.json({message:"Analyze endpoint hit", resume:resume, jobDescription:jobDes});
+
+
+    
 })
 
 
