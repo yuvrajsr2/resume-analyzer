830e12ae96d0419e2bba84496a826f6fb37dc7fe
Yuvraj Singh Rajput <75319329+yuvrajsr2@users.noreply.github.com>
2025-10-23 15:01:17 -0400

    Changed storage type from disk to memory buffer so we wont have to delete the files manually

--- backend/src/utils/uploadHandle.js
+++ backend/src/utils/uploadHandle.js
@@ -2,17 +2,7 @@ import multer from 'multer';
 import path from 'path';
 
 
-const storage = multer.diskStorage({
-    destination:function(req, file, cb){
-        cb(null, 'uploads/')
-
-    },
-
-    filename: (req, file, cb) => {
-        const uniqueName = Date.now() + '-' + file.originalname;
-        cb(null, uniqueName);
-    }
-})
+const storage = multer.memoryStorage();
 
 
 const filter = (req, file, cb) =>{
