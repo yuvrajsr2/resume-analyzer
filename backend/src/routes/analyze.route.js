13627a88aea33676032270de838bcb94c99f73ed
Yuvraj Singh Rajput <75319329+yuvrajsr2@users.noreply.github.com>
2025-10-23 12:31:12 -0400

    Documenting

--- backend/src/routes/analyze.route.js
+++ backend/src/routes/analyze.route.js
@@ -14,6 +14,10 @@ router.post('/upload', upload, (req, res) =>{
 
 
 // analyze endpoint:todo using hugging face api to analyze the resume and job description
+//todo:take in the uploaded files from the request like req.files.resume and req.files.jobDescription and
+// process them accordingly and use the hugging face api to analyze them. and then return 
+// the analysis result as json response. the response should be a percetange match 
+//and the the key skills matched and not matched and give recommendation to improve the resume.
 router.post('/analyze', (req, res) =>{
     res.send("Analyze endpoint");
 })
