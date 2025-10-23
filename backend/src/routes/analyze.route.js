3a247730b0bb4bd7db93f9cbf374dac02de1ab83
Yuvraj Singh Rajput <75319329+yuvrajsr2@users.noreply.github.com>
2025-10-23 00:18:30 -0400

    Adding basic routes

---
+++ backend/src/routes/analyze.route.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+
+const router = express.Router();
+
+router.post('/', (req, res) =>{
+    res.send("Upload endpoint");
+})
+
+
+router.post('/api/upload', (req, res) =>{
+    res.send("Upload endpoint");
+})
--- backend/src/server.js
+++ backend/src/server.js
@@ -3,11 +3,14 @@ import cors from 'cors';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
 import path from 'path';
+import analyzeRoute from './routes/analyze.route.js';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT;
+
+app.use("/api/analyze", analyzeRoute)
 
 
 app.listen(PORT, ()=>{
