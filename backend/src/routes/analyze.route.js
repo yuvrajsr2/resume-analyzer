79560f5fb91ac266363e3cb829a386d1e436e10e
Yuvraj Singh Rajput <75319329+yuvrajsr2@users.noreply.github.com>
2025-10-24 16:24:04 -0400

    Added the hugging face api analyzing

--- backend/src/routes/analyze.route.js
+++ backend/src/routes/analyze.route.js
@@ -1,13 +1,16 @@
 import express from 'express';
 import multer from 'multer';
-import {upload} from '../utils/uploadHandle.js';
+import { upload } from '../utils/uploadHandle.js';
 import axios from 'axios';
-import {PDFParse} from "pdf-parse";
+import { PDFParse } from "pdf-parse";
+import { InferenceClient } from '@huggingface/inference';
+
 
 
 
 
 const router = express.Router();
+const client = new InferenceClient({ accessToken: process.env.HF_TOKEN });
 
 
 // upload uysing multer
@@ -23,51 +26,82 @@ const router = express.Router();
 // process them accordingly and use the hugging face api to analyze them. and then return 
 // the analysis result as json response. the response should be a percetange match 
 //and the the key skills matched and not matched and give recommendation to improve the resume.
-router.post('/analyze', upload, async (req, res) =>{
+router.post('/analyze', upload, async (req, res) => {
+
+    try {
+
+        if (!req.files || !req.files["resume"] || !req.files["jobDescription"]) {
+            return res.status(400).json({ error: "Invalid plz enter both files gang" })
+        }
 
 
-    if (!req.files || !req.files["resume"] || !req.files["jobDescription"]){
-        return res.status(400).json({error:"Invalid plz enter both files gang"})
+        const resume = req.files['resume'][0];
+        const jobDes = req.files['jobDescription'][0];
+
+
+        const extractData = async (data) => {
+            if (data.mimetype === "application/pdf") {
+                const p = new PDFParse({ data: data.buffer });
+                const res = await p.getText();
+                await p.destroy();
+                return res.text;
+            }
+            else {
+                return data.buffer.toString("utf-8");
+            }
+        }
+
+
+        const resumeData = await extractData(resume);
+        const jobData = await extractData(jobDes);
+
+
+
+        //todo: get hugging face api, then send a axios req to hugging face with the prompt
+        const prompt = `
+    You are a resume analyzer. Compare the following resume and job description.
+    Return:
+    - A percentage match (0–100%)
+    - 5 key matching skills
+    - 5 missing or weak skills
+    - 3 short recommendations to improve the resume.
+
+    Resume:
+    ${resumeData}
+
+    Job Description:
+    ${jobData}
+    `;
+
+
+        const completion = await client.chatCompletion({
+            model: 'deepseek-ai/DeepSeek-V3-0324',
+            provider: "auto",
+            messages: [{
+                role: "user",
+                content: prompt,
+            },
+            ],
+        });
+
+        const response = completion.choices[0].message.content;
+
+        // return formated res
+        res.json({
+            success: true,
+            analysis: response,
+        });
+
+
+
+        console.log("Hugging face response:", response);
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Internal error", details: error.message });
     }
 
 
-    const resume = req.files['resume'][0];
-    const jobDes = req.files['jobDescription'][0];
-
-
-    const extractData = async (data)=>{
-        if (data.mimetype === "application/pdf"){
-            const p = new PDFParse({data:data.buffer});
-            const res = await p.getText();
-            await p.destroy();
-            return res.text;
-        }
-        else{
-            return data.buffer.toString("utf-8");
-        }
-    }
-
-
-    const resumeData = await extractData(resume);
-    const jobData = await extractData(jobDes);
-
-
-
-    //todo: get hugging face api, then send a axios req to hugging face with the prompt
-    
-
-
-    // return formated res
-
-   
-
-
-
-    res.json({message:"Analyze endpoint hit", resume:resumeData, jobDescription:jobData});
-    console.log(resumeData, jobData);
-
-
-    
 })
 
 
