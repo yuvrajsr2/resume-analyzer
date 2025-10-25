8fb2e4988f6c96c01d12b552c2d39c4bb0481c74
Yuvraj Singh Rajput <75319329+yuvrajsr2@users.noreply.github.com>
2025-10-25 16:38:21 -0400

    Documentation

--- frontend/src/pages/analyzePage.jsx
+++ frontend/src/pages/analyzePage.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 
 
-
+//Analyze page component
+// things to build here: display analysis results, provide options to download or share results
+// styling and layout with ResumeAnalyzer theme and header on top
+// maybe a back button to go back to home page
 const AnalyzePage = () =>{
     return(
         <div>
             Analyze page
+            
         </div>
     )
 }
--- frontend/src/pages/homePage.jsx
+++ frontend/src/pages/homePage.jsx
@@ -1,5 +1,13 @@
 import React from 'react'
 
+
+
+// Home page component
+// things to build here:
+// 1. Upload resume button and upload job descirption button
+// 2. analyze button that takes to analyze page with results
+// 3. also be able to enter text directly instead of uploading files
+// styling and layout with ResumeAnalyzer theme and header on top
 const HomePage = () =>{
     return(
         <div>
